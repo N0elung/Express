498038b99670d26f01f7f28cf907ad0a08a62169
N0elung <noelb.davalos@gmail.com>
2024-11-09 14:23:53 -0800

    Editar

---
+++ src/components/FormEditar.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function FormEditar() {
+  return (
+    <table>
+        <tr>
+            <th>ID</th>
+            <th>Nombre</th>
+            <th>Genero</th>
+            <th>Descripcion</th>
+            <th>Accion</th>
+        </tr>
+        <tr>
+            
+            <td></td>
+        </tr>
+    </table>
+  )
+}
+
+export default FormEditar
